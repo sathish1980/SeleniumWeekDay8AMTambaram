7a2cde87629ddfaa300d03a97c5f22ab8d2e2e33
sathish1980 <kumar.sathish189@gmail.com>
2023-09-28 09:11:53 +0530

    locators -id

--- src/test/java/SeleniumBasics/FirstClass.java
+++ src/test/java/SeleniumBasics/FirstClass.java
@@ -1,13 +1,15 @@
 package SeleniumBasics;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FirstClass {
 	
 	
-	public void launch() throws InterruptedException
+	public void launchBrowser() throws InterruptedException
 	{
 		//WebDriver browser = new ChromeDriver();
 		WebDriver browser = new EdgeDriver();
@@ -15,17 +17,22 @@ public class FirstClass {
 		//Thread.sleep(2000);
 		browser.get("https://www.facebook.com/");
 		Thread.sleep(2000);
-		browser.navigate().to("https://www.gmail.com/");
+		/*browser.navigate().to("https://www.gmail.com/");
 		//browser.quit();
 		browser.navigate().back();
 		browser.navigate().forward();
-		browser.navigate().refresh();
+		browser.navigate().refresh();*/
+		WebElement userName = browser.findElement(By.id("email"));
+		userName.sendKeys("sathish");
+		userName.clear();
+		//or
+		browser.findElement(By.id("email")).sendKeys("Kumar");
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		FirstClass F = new FirstClass();
-		F.launch();
+		F.launchBrowser();
 	}
 
 }
