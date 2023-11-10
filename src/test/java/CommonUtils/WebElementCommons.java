7e233567f9790df3b0a69ca5211da71cdc02177b
sathish1980 <kumar.sathish189@gmail.com>
2023-11-10 09:27:04 +0530

    makemytrip

--- src/test/java/CommonUtils/WebElementCommons.java
+++ src/test/java/CommonUtils/WebElementCommons.java
@@ -1,6 +1,19 @@
 package CommonUtils;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Date;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class WebElementCommons {
 
@@ -31,6 +44,165 @@ public class WebElementCommons {
 		}
 		return null;
 	}
+
+	public void ClickOnBrowserBackButton(WebDriver driver)
+	{
+		driver.navigate().back();
+	}
+
+	public void WaitForElementToBeVisible(WebDriver driver, By WaitForElement, int secondsToWait)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(secondsToWait));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(WaitForElement));
+	}
+
+	public void WaitForElementToBeClickable(WebDriver driver, By WaitForElement, int secondsToWait)
+	{
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(secondsToWait));
+		wait.until(ExpectedConditions.elementToBeClickable(WaitForElement));
+	}
+
+	public void SelectTheValueFromList(WebDriver driver, By AllListelement,String expectedloaction)
+	{
+		List<WebElement> AllValues = driver.findElements(AllListelement);
+		for (WebElement eachElement : AllValues )
+		{
+			String actualFromLocation = eachElement.findElement(By.cssSelector("div[class*='makeFlex']>div[class*='pushRight']")).getText();
+			if(expectedloaction.equalsIgnoreCase(actualFromLocation))
+			{
+				eachElement.click();
+				break;
+			}
+		}
+	}
+
+
+	public void ClickOnButton(WebElement buttonElement)
+	{
+		if (buttonElement.isEnabled())
+			buttonElement.click();
+	}
+
+	public void CloseCreateAccountPopup(WebElement buttonElement)
+	{
+		try
+		{
+			if (buttonElement.isEnabled())
+				buttonElement.click();
+		}
+		catch(Exception e)
+		{
+
+		}
+	}
+
+	public boolean SelectValueinCalender(WebDriver driver,String expectedDay)
+	{
+		List<WebElement> totalWeekRows = driver.findElements(By.xpath("(//*[@class='DayPicker-Months']//div)[1]//div[@class='DayPicker-Week']"));
+		for(WebElement eachWeekRow : totalWeekRows)
+		{
+			List<WebElement> totalDaysInWeek = eachWeekRow.findElements(By.cssSelector("div[class^='DayPicker-Day']"));
+			for(WebElement eachDays : totalDaysInWeek)
+			{
+				String checkItsDisabled = eachDays.getAttribute("class");
+				if (!checkItsDisabled.contains("disabled"))
+				{
+					String eachDayValue =eachDays.findElement(By.cssSelector("[class='dateInnerCell']>p:nth-of-type(1)")).getText();
+					if(eachDayValue.equalsIgnoreCase(expectedDay))
+					{
+						eachDays.click();
+						return true;
+					}
+
+				}
+			}
+		}
+		return false;
+	}
+
+	public void clickOnButton(WebElement buttonElement)
+	{
+
+		if (buttonElement.isDisplayed())
+		{
+			buttonElement.click();
+		}
+	}
+
+	public String GetTextOfelement(WebElement getTextElement)
+	{
+		if(getTextElement.isDisplayed())
+			return getTextElement.getText();
+		return null;
+	}
+
+	public String GetAttributeOfelement(WebElement getElement,String attribute)
+	{
+		if(getElement.isDisplayed())
+			return getElement.getAttribute(attribute);
+		return null;
+	}
+
+	public void EnterText(WebElement getTextElement, String textToEnter)
+	{
+		if(getTextElement.isDisplayed())
+			getTextElement.sendKeys(textToEnter);
+	}
+
+	public void ClickIfElementExist(WebDriver driver,String Elementpath) {
+		List<WebElement>allframeelement = driver.findElements(By.tagName("iframe"));
+		for(int i=0;i<allframeelement.size();i++)
+		{
+			driver.switchTo().frame(i);
+			List<WebElement> AllElements = driver.findElements(By.xpath(Elementpath));
+			if (AllElements.size() > 0) {
+				for (WebElement eachElement : AllElements) {
+
+					eachElement.click();
+					driver.switchTo().defaultContent();
+					break;
+				}
+			}
+			else
+			{
+				driver.switchTo().defaultContent();
+			}
+		}
+	}
+
+	public void ClickOnAddClose(WebDriver driver)
+	{
+		try
+		{
+			WaitForElementToBeClickable(driver,By.xpath("//*[@class='ic_circularclose_grey']"),60);
+			driver.findElement(By.xpath("//*[@class='ic_circularclose_grey']")).click();
+		}
+		catch(Exception e)
+		{
+
+		}
+	}
+
+	public static String takescreenshot(WebDriver driver)
+	{
+		Date date = new Date();
+		//This method returns the time in millis
+		long timeMilli = date.getTime();
+		String date_v= String.valueOf(timeMilli);
+		//System.out.println(date_v);
+		TakesScreenshot ts =(TakesScreenshot)driver;
+		File sourcefile= ts.getScreenshotAs(OutputType.FILE);
+		File Destinationfile= new File(System.getProperty("user.dir")+"//Screenshot//"+date_v+".png");
+		try {
+			FileUtils.copyFile(sourcefile, Destinationfile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return Destinationfile.toString();
+	}
+
 
 }
 
--- src/test/java/Testcases/MakeMyTripFlighSearch.java
+++ src/test/java/Testcases/MakeMyTripFlighSearch.java
@@ -2,15 +2,22 @@ package Testcases;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import BrowserDriver.Browser;
+import CommonUtils.WebElementCommons;
 import Utils.PropertyFileRead;
 
+
 public class MakeMyTripFlighSearch extends Browser{
+
+	WebElementCommons c = new WebElementCommons();
 
 	@BeforeSuite
 	public void LaunchTheBrowser() throws IOException
@@ -23,12 +30,103 @@ public class MakeMyTripFlighSearch extends Browser{
 	{
 		String applicationURL =PropertyFileRead.GetEnvironmentDetail().getProperty("url");
 		driver.get(applicationURL);
+		//driver.navigate().refresh();
+	}
+	@BeforeClass
+	public void deleteAdds() throws InterruptedException
+	{
+		Thread.sleep(3000);
+		c.ClickIfElementExist(driver,"//*[@data-cy='closeModal']");
+		Thread.sleep(1000);
+		c.ClickIfElementExist(driver,"//*[@class='close']");
+		Thread.sleep(2000);
+		c.CloseCreateAccountPopup(driver.findElement(By.xpath("//*[@data-cy='closeModal']")));
+		Thread.sleep(2000);
+		//c.ClickOnAddClose(driver);
+
+		//Thread.sleep(2000);
 	}
 
-	@Test
-	public void sampleTestcase()
+	@Test(priority=0)
+	public void SearchWithValidValues()
 	{
-		System.out.println("This is first Testcae");
+
+		/*
+		 * Click on From Location
+		 * Click on To Location
+		 * Select the Date
+		 * click on search Button
+		 * Click Ok in the upcoming popup
+		 * Get the search result text and validate with the input
+		 */
+		//Select From location
+		c.WaitForElementToBeClickable(driver,By.xpath("//*[@for='fromCity']"),60);
+		c.ClickOnButton(driver.findElement(By.xpath("//*[@for='fromCity']")));
+
+
+		//WaitForElementToBeVisible(driver,By.xpath("(//ul[@role='listbox']//li)[1]"),60);
+		c.WaitForElementToBeVisible(driver,By.xpath("(//ul[@role='listbox']//li)[1]"),60);
+		c.SelectTheValueFromList(driver, By.xpath("//ul[@role='listbox']//li"),"MAA");
+		String FromCityName=c.GetAttributeOfelement(driver.findElement(By.xpath("//input[@id='fromCity']")),"value");
+		//Select To Location
+
+		//WebElement toButton = driver.findElement(By.xpath("//*[@for='toCity']"));
+		c.ClickOnButton(driver.findElement(By.xpath("//*[@for='toCity']")));
+		//ClickOnButton(toField);
+
+		c.SelectTheValueFromList(driver, By.xpath("//ul[@role='listbox']//li"),"BLR");
+		String ToCityName=c.GetAttributeOfelement(driver.findElement(By.xpath("//input[@id='toCity']")),"value");
+		//Select Date
+		c. SelectValueinCalender(driver,"18");
+		//click on Search Button
+		c.clickOnButton(driver.findElement(By.xpath("//*[@data-cy='submit']//a")));
+		//Wait and click on ok in the Popopup
+		c.WaitForElementToBeClickable(driver,By.xpath("//*[text()='OKAY, GOT IT!']"),60);
+		c.clickOnButton(driver.findElement(By.xpath("//*[text()='OKAY, GOT IT!']")));
+		// get the search Result:
+		String actualSearchResult =c.GetTextOfelement(driver.findElement(By.xpath("//*[contains(@class,'journey-title')]")));
+		System.out.println(actualSearchResult);
+		String expectedSearchResult = "Flights from "+FromCityName+" to "+ToCityName;
+		Assert.assertEquals(actualSearchResult, expectedSearchResult);	driver.navigate().back();
+	}
+
+	/*Created By : Sathish kumar R
+	 * Created On :10-11-2023
+	 * REQ NO :3
+	 * TC NO :12
+	 *
+	 */
+	@Test(priority=1)
+	public void SearchWithSameFromAndToLocation() throws InterruptedException
+	{
+
+		/*
+		 * Click on From Location and select a value
+		 * Select same as From in To Location
+		 * Validate "From & To airports cannot be the same" error is displayed
+		 */
+		//Click on back button in the browser
+		//driver.navigate().back();
+
+		Thread.sleep(5000);
+		c.ClickIfElementExist(driver,"//*[@class='close']");
+		Thread.sleep(2000);
+		//c.CloseCreateAccountPopup(driver.findElement(By.xpath("//*[@data-cy='closeModal']")));
+		//Thread.sleep(2000);
+
+		//Select To Location
+
+		//WebElement toButton = driver.findElement(By.xpath("//*[@for='toCity']"));
+		c.ClickOnButton(driver.findElement(By.xpath("//*[@for='toCity']")));
+		//ClickOnButton(toField);
+
+		c.SelectTheValueFromList(driver, By.xpath("//ul[@role='listbox']//li"),"MAA");
+		String ToCityName=c.GetAttributeOfelement(driver.findElement(By.xpath("//input[@id='toCity']")),"value");
+		c.WaitForElementToBeVisible(driver, By.cssSelector("[data-cy='sameCityError']"), 60);
+		String actualErrorMessage =c.GetTextOfelement(driver.findElement(By.cssSelector("[data-cy='sameCityError']")));
+		String expectedErrorMessage ="From & To airports cannot be the same";
+		Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+
 	}
 
 	@AfterSuite
